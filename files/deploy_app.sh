39067813195b52842375dc2bc40d074f6723b15b
FangliZhan <31291225+FangliZhan@users.noreply.github.com>
2022-06-17 11:11:23 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. IaC is so cool. 
+  Welcome to ${PREFIX}'s Hashicat app. Terraform Cloud is pretty cool. 
   <!-- END -->
   
   </div>
